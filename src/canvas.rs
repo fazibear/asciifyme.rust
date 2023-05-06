93ff60724fffda816435ccd8a32b638b4eb2e9a0
Michał Kalbarczyk <fazibear@gmail.com>
2023-05-07 00:20:44 +0200

    more cleanups

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,7 +1,7 @@
 use wasm_bindgen::{prelude::*, Clamped};
 use web_sys::{window, CanvasRenderingContext2d, HtmlCanvasElement, HtmlVideoElement};
 
-use crate::{CANVAS_WIDTH, CANVAS_HEIGHT};
+use crate::{CANVAS_HEIGHT, CANVAS_WIDTH};
 
 #[derive(Debug)]
 pub struct Canvas {
@@ -12,32 +12,31 @@ impl Canvas {
     pub fn new() -> Self {
         let document = window().unwrap().document().unwrap();
 
+        let mut context_attributes = web_sys::ContextAttributes2d::new();
+        context_attributes.will_read_frequently(true);
+
         let context = document
             .create_element("canvas")
             .unwrap()
             .dyn_into::<HtmlCanvasElement>()
             .unwrap()
-            .get_context("2d")
+            .get_context_with_context_options("2d", &context_attributes)
             .unwrap()
             .unwrap()
             .dyn_into::<CanvasRenderingContext2d>()
             .unwrap();
 
-        Self {
-            context
-        }
+        Self { context }
     }
 
     pub fn draw_image(self: &Self, video: &HtmlVideoElement) {
-	    self
-            .context
+        self.context
             .draw_image_with_html_video_element(video, CANVAS_WIDTH as f64, CANVAS_HEIGHT as f64)
             .unwrap();
     }
 
-    pub fn get_image_data(self: &Self) -> Clamped<Vec<u8>>{
-        self
-            .context
+    pub fn get_image_data(self: &Self) -> Clamped<Vec<u8>> {
+        self.context
             .get_image_data(0.0, 0.0, CANVAS_HEIGHT as f64, CANVAS_WIDTH as f64)
             .unwrap()
             .data()
